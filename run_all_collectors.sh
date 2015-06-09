89d15e3e0417539840cde74c3c348c080a9bf631
Saurabh Hirani <saurabhhirani@bluejeansnet.com>
2015-06-09 19:52:35 +0530

    updated run_all_collectors to use env file containing vars for run_all_collectors.sh

--- run_all_collectors.sh
+++ run_all_collectors.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+
+env_file='collectors_env_vars.sh'
+
+if ! [ -f $env_file ]; then
+  echo "ERROR: Did not find env file $env_file. Create one export vars used in this script"
+  exit 1
+fi
+
+. $env_file
+
 echo flushdb | redis-cli
+
 python run_collectors.py chef.main --view env $*
+
 python run_collectors.py nagios.host_status $*
+
 python run_collectors.py chef.main --view node $*
+
 python run_collectors.py aws.main --view region --region us-west-1 $*
+
 python run_collectors.py aws.main --view region --region us-west-2 $*
+
 python run_collectors.py aws.main --view region --region us-east-1 $*
-python run_collectors.py device42.main -p $DEVICE42_PASSWD --service_levels Stage Production $*
+
+python run_collectors.py device42.main --username $DEVICE42_USERNAME --password $DEVICE42_PASSWD --service_levels Stage Production $*
+
+python run_collectors.py netscalar.main --view lbvserver --username $NETSCALAR_USERNAME --password $NETSCALAR_PASSWD --host $DEVICE42_HOST $*
