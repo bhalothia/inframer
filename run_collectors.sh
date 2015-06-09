c6322ca74fdab4af10c0571416c7503eac49993e
Saurabh Hirani <saurabhhirani@bluejeansnet.com>
2015-06-09 20:04:00 +0530

    fixing run_collector.py call

--- run_collectors.sh
+++ run_collectors.sh
@@ -11,18 +11,18 @@ fi
 
 echo flushdb | redis-cli
 
-python run_collectors.py chef.main --view env $*
+python run_collector.py chef.main --view env $*
 
-python run_collectors.py nagios.host_status $*
+python run_collector.py nagios.host_status $*
 
-python run_collectors.py chef.main --view node $*
+python run_collector.py chef.main --view node $*
 
-python run_collectors.py aws.main --view region --region us-west-1 $*
+python run_collector.py aws.main --view region --region us-west-1 $*
 
-python run_collectors.py aws.main --view region --region us-west-2 $*
+python run_collector.py aws.main --view region --region us-west-2 $*
 
-python run_collectors.py aws.main --view region --region us-east-1 $*
+python run_collector.py aws.main --view region --region us-east-1 $*
 
-python run_collectors.py device42.main --username $DEVICE42_USERNAME --password $DEVICE42_PASSWD --service_levels Stage Production $*
+python run_collector.py device42.main --username $DEVICE42_USERNAME --password $DEVICE42_PASSWD --service_levels Stage Production $*
 
-python run_collectors.py netscalar.main --view lbvserver --username $NETSCALAR_USERNAME --password $NETSCALAR_PASSWD --host $DEVICE42_HOST $*
+python run_collector.py netscalar.main --view lbvserver --username $NETSCALAR_USERNAME --password $NETSCALAR_PASSWD --host $DEVICE42_HOST $*
